a8e698f48b7c13fe66234ed696b698df6a0f6085
MarKru21 <77971128+MarKru21@users.noreply.github.com>
2021-04-21 18:39:31 +0200

    added the weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
         date: new Date(response.data.dt * 1000),
         city: response.data.name,
         humidity: response.data.main.humidity,
-        iconURL: `https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`,
+        iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         description: response.data.weather[0].description,
     });
 }
